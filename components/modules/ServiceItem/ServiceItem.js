92e80cedca366c4c63861985038dd269a54d1257
mohammadfaramarzi1 <faramarzi.dev@gamil.com>
2025-03-08 15:47:37 +0330

    feat: Services template

--- components/modules/ServiceItem/ServiceItem.js
+++ components/modules/ServiceItem/ServiceItem.js
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from "react";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import { config } from "@fortawesome/fontawesome-svg-core";
+config.autoAddCss = false;
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import * as Icons from "@fortawesome/free-solid-svg-icons";
 
-function ServiceItem() {
+function ServiceItem({ title, desc, img, icon }) {
   return (
-    <div>ServiceItem</div>
-  )
+    <div className="col-lg-6 mb-5">
+      <div className="row align-items-center">
+        <div className="col-sm-5">
+          <img className="img-fluid mb-3 mb-sm-0" src={img} alt="" />
+        </div>
+        <div className="col-sm-7">
+          <div style={{ display: "flex", alignItems: "center" }}>
+            <div
+              style={{
+                background: "#da9f5b",
+                borderRadius: "100%",
+                height: "35px",
+                width: "40px",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <FontAwesomeIcon icon={Icons[icon]} style={{ fontSize: "1rem" }} />
+            </div>
+            <p className="ml-3">{title}</p>
+          </div>
+          <p className="m-0">{desc}</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default ServiceItem
+export default ServiceItem;
