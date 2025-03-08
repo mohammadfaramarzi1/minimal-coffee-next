7a08b40a76f1d5648e29577b2658ef84a791758d
mohammadfaramarzi1 <faramarzi.dev@gamil.com>
2025-03-08 16:12:14 +0330

    add: db.json

--- components/templates/Index/About.js
+++ components/templates/Index/About.js
@@ -1,4 +1,9 @@
 import React from "react";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import { config } from "@fortawesome/fontawesome-svg-core";
+config.autoAddCss = false;
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheck } from "@fortawesome/free-solid-svg-icons";
 
 function About() {
   return (
@@ -33,12 +38,12 @@ function About() {
               Learn More
             </a>
           </div>
-          <div className="col-lg-4 py-5 py-lg-0" style={{minHeight: "500px"}}>
+          <div className="col-lg-4 py-5 py-lg-0" style={{ minHeight: "500px" }}>
             <div className="position-relative h-100">
               <img
                 className="position-absolute w-100 h-100"
                 src="/img/about.png"
-                style={{objectFit: "cover"}}
+                style={{ objectFit: "cover" }}
               />
             </div>
           </div>
@@ -51,18 +56,21 @@ function About() {
               dolor
             </p>
             <h5 className="mb-3">
-              <i className="fa fa-check text-primary mr-3"></i>Lorem ipsum dolor sit
-              amet
+              <FontAwesomeIcon icon={faCheck} className="text-primary mr-3" />
+              Lorem ipsum dolor sit amet
             </h5>
             <h5 className="mb-3">
-              <i className="fa fa-check text-primary mr-3"></i>Lorem ipsum dolor sit
-              amet
+              <FontAwesomeIcon icon={faCheck} className="text-primary mr-3" />
+              Lorem ipsum dolor sit amet
             </h5>
             <h5 className="mb-3">
-              <i className="fa fa-check text-primary mr-3"></i>Lorem ipsum dolor sit
-              amet
+              <FontAwesomeIcon icon={faCheck} className="text-primary mr-3" />
+              Lorem ipsum dolor sit amet
             </h5>
-            <a href="" className="btn btn-primary font-weight-bold py-2 px-4 mt-2">
+            <a
+              href=""
+              className="btn btn-primary font-weight-bold py-2 px-4 mt-2"
+            >
               Learn More
             </a>
           </div>
