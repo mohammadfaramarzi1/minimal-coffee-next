5090831b52b451ca3036151562b91e43d21ccb94
mohammadfaramarzi1 <faramarzi.dev@gamil.com>
2025-03-09 11:35:42 +0330

    feat: Services page template

--- components/templates/ServicesDetails/ServicesDetails.js
+++ components/templates/ServicesDetails/ServicesDetails.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import ServiceItem from "@/components/modules/ServiceItem/ServiceItem";
 
-function ServicesDetails() {
+function ServicesDetails({ services }) {
   return (
-    <div>ServicesDetails</div>
-  )
+    <div className="container-fluid pt-5">
+      <div className="container">
+        <div className="section-title">
+          <h4
+            className="text-primary text-uppercase"
+            style={{ letterSpacing: "5px" }}
+          >
+            Our Services
+          </h4>
+          <h1 className="display-4">Fresh &amp; Organic Beans</h1>
+        </div>
+        <div className="row">
+          {services.map((service) => (
+            <ServiceItem key={service.id} {...service} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default ServicesDetails
+export default ServicesDetails;
--- pages/services/index.js
+++ pages/services/index.js
@@ -1,7 +1,28 @@
+import Header from "@/components/modules/Header/Header";
 import React from "react";
+import AllServices from "@/components/templates/Index/Services";
+import ServicesDetails from "@/components/templates/ServicesDetails/ServicesDetails";
 
-function Services() {
-  return <div>Services</div>;
+function Services({ data }) {
+  return (
+    <>
+      <Header route="Services" />
+      <ServicesDetails services={data.services} />
+    </>
+  );
+}
+
+export async function getStaticProps() {
+  const res = await fetch("http://localhost:4000/services");
+  const services = await res.json();
+
+  return {
+    props: {
+      data: {
+        services,
+      },
+    },
+  };
 }
 
 export default Services;
