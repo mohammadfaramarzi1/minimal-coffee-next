a6747e46a51992ee8aec4d774eb6cc8083da8b64
mohammadfaramarzi1 <faramarzi.dev@gamil.com>
2025-03-09 11:49:43 +0330

    feat: Menu page template

--- components/templates/Menu/Pricing.js
+++ components/templates/Menu/Pricing.js
@@ -1,9 +1,42 @@
-import React from 'react'
+import Card from "@/components/modules/Card/Card";
+import React from "react";
 
-function Pricing() {
+function Pricing({menu}) {
   return (
-    <div>Pricing</div>
-  )
+    <div className="container-fluid pt-5">
+      <div className="container">
+        <div className="section-title">
+          <h4
+            className="text-primary text-uppercase"
+            style={{ letterSpacing: "5px" }}
+          >
+            Menu & Pricing
+          </h4>
+          <h1 className="display-4">Competitive Pricing</h1>
+        </div>
+        <div className="row">
+          <div className="col-lg-6">
+            <h1 className="mb-5">Hot Coffee</h1>
+            {menu
+              .filter((item) => item.type === "hot")
+              .slice(0, 3)
+              .map((item) => (
+                <Card key={item.id} {...item} />
+              ))}
+          </div>
+          <div className="col-lg-6">
+            <h1 className="mb-5">Cold Coffee</h1>
+            {menu
+              .filter((item) => item.type === "cold")
+              .slice(0, 3)
+              .map((item) => (
+                <Card key={item.id} {...item} />
+              ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Pricing
+export default Pricing;
--- pages/menu/index.js
+++ pages/menu/index.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import Header from "@/components/modules/Header/Header";
+import Pricing from "@/components/templates/Menu/Pricing";
+import React from "react";
 
-function Menu() {
+function Menu({ data }) {
   return (
-    <div>Menu</div>
-  )
+    <>
+      <Header route="Menu" />
+      <Pricing menu={data.menu} />
+    </>
+  );
 }
 
-export default Menu
+export async function getStaticProps() {
+  const res = await fetch("http://localhost:4000/menu");
+  const menu = await res.json();
+
+  return {
+    props: {
+      data: {
+        menu,
+      },
+    },
+    revalidate: 6 * 6 * 12,
+  };
+}
+
+export default Menu;
